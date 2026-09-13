f8072cdcdacd070b110c41da6d26fd25a4180af2
valentinchernyak <164994021+valentinchernyak@users.noreply.github.com>
2026-09-13 15:46:00 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig(({ command }) => ({
   // Project Pages are served from https://<owner>.github.io/GenderGap/.
-  base: command === 'build' ? '/GenderGap/' : '/',
+  base: command === 'build' ? '/russian-partner-calculator/' : '/',
   plugins: [react()],
 }));
